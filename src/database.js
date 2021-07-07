94eac2d68b2207428b15591de2b58f6cbb1124f5
robertoalonsogandia <roberto.alonso.gandia@gmail.com>
2021-07-07 07:59:28 +0200

    mlab ha sido agregado 3

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://roberto:<password>@cluster0.pisul.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://roberto:Adosat2020@cluster0.pisul.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
     useNewUrlParser: true
 })
   .then(db => console.log('db connected'))
