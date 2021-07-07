6238b16d817d2cd1ebf3a1d68c9e000f3f31c5b4
robertoalonsogandia <roberto.alonso.gandia@gmail.com>
2021-07-07 07:57:51 +0200

    mlab ha sido agregado 2

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://roberto:Adosat2020@cluster0.pisul.mongodb.net/test?authSource=admin&replicaSet=atlas-4y161u-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true', {
+mongoose.connect('mongodb+srv://roberto:<password>@cluster0.pisul.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
     useNewUrlParser: true
 })
   .then(db => console.log('db connected'))
